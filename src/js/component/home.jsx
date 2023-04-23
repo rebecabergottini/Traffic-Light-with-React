fa52e2d94957387bb607c235fffb10eb49fef92d
rebecabergottini <117315880+rebecabergottini@users.noreply.github.com>
2023-04-23 19:51:15 +0000

    final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [colorBtnRed, setColorBtnRed] = useState("btn-danger");
+  const [colorBtnYellow, setColorBtnYellow] = useState("btn-warning");
+  const [colorBtnGreen, setColorBtnGreen] = useState("btn-success");
+
+
+function changeColor(color) {
+  switch (color) {
+    case "red":
+      setColorBtnRed("btn-danger active");
+      setColorBtnYellow("btn-warning");
+      setColorBtnGreen("btn-success");
+      break;
+    case "yellow":
+      setColorBtnRed("btn-danger");
+      setColorBtnYellow("btn-warning active");
+      setColorBtnGreen("btn-success");
+      break;
+    case "green":
+      setColorBtnRed("btn-danger");
+      setColorBtnYellow("btn-warning");
+      setColorBtnGreen("btn-success active");
+      break;
+    default:
+      setColorBtnRed("btn-danger");
+      setColorBtnYellow("btn-warning");
+      setColorBtnGreen("btn-success");
+  }
+}
+  
+return (
+  <div className="container">
+      <div className="pole"></div>
+    <div className="traffic-light">
+      <div className={"btn " + colorBtnRed} onClick={() => changeColor("red")}></div>
+      <div className={"btn " + colorBtnYellow} onClick={() => changeColor("yellow")}></div>
+      <div className={"btn " + colorBtnGreen} onClick={() => changeColor("green")}></div>
+    </div>
+  </div>
+);
 };
 
 export default Home;
